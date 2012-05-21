f68d49cc2633ff914fb41b2e0ede21e8b7ea62d6
aem999 <aem999.github@gmail.com>
2012-05-21 10:14:18 +0800

    Solved problem #1 using calculus

--- src/main/java/com/aem999/euler/Problem1.java
+++ src/main/java/com/aem999/euler/Problem1.java
@@ -5,13 +5,8 @@ import org.slf4j.LoggerFactory;
 
 public class Problem1 {
     private static final Logger LOG = LoggerFactory.getLogger(Problem1.class);
-    private final int maxNumber;
 
-    public Problem1(int maxNumber) {
-        this.maxNumber = maxNumber;
-    }
-
-    public long solve() {
+    public long solve(int maxNumber) {
         long sum = 0;
         for (int i = 3; i < maxNumber; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
@@ -21,5 +16,66 @@ public class Problem1 {
         }
         LOG.info("sum=[{}]", sum);
         return sum;
+    }
+
+    public long solveUsingCalculus(int maxNumber) {
+        return findSumOfTerms(3, maxNumber) +
+               findSumOfTerms(5, maxNumber) -
+               findSumOfTerms(15, maxNumber);
+    }
+
+    private long findSumOfTerms(int d, int maxNumber) {
+        int lastTerm = findLastTerm(d, maxNumber);
+        int noOfTerms = findNumberOfTerms(d, lastTerm, d);
+        return findSumOfTerms(d, lastTerm, noOfTerms);
+    }
+
+    /**
+     * Returns the last term in the arithmetic sequence with difference {@code d},
+     * up to but not including {@code maxNumber}
+     */
+    public int findLastTerm(int divisor, int maxNumber) {
+        return (maxNumber - 1) / divisor * divisor;
+    }
+
+    /**
+     * To find the sum of a certain number of terms of an arithmetic sequence:
+     *
+     * <pre>
+     * S<sub>n</sub> = n(a<sub>1</sub> + a<sub>n</sub>) / 2</pre>
+     *
+     * where:
+     *
+     * <pre>
+     * S<sub>n</sub> is the sum of n terms (nth partial sum),
+     * a<sub>1</sub> is the first term
+     * a<sub>n</sub> is the nth term</pre>
+     *
+     * @return the sum of the terms
+     */
+    public long findSumOfTerms(int a1, int an, int n) {
+        return n * (a1 + an) / 2;
+    }
+
+    /**
+     * To find any term of an arithmetic sequence:
+     *
+     * <pre>
+     * a<sub>n</sub> = a<sub>1</sub> + (n - 1)d</pre>
+     *
+     * so to find the number of terms in an arithmetic sequence:
+     *
+     * <pre>
+     * n = (a<sub>n</sub> - a<sub>1</sub> + d) / d</pre>
+     *
+     * where:
+     *
+     * <pre>
+     * a1 is the first term of the sequence,
+     * d is the common difference
+     * n is the number of the term to find.</pre>
+     */
+    public int findNumberOfTerms(int a1, int an, int d) {
+        return (an - a1 + d) / d;
     }
 }
--- src/test/java/com/aem999/euler/Problem1Test.java
+++ src/test/java/com/aem999/euler/Problem1Test.java
@@ -9,8 +9,33 @@ public class Problem1Test {
 
     @Test
     public void solveProblem() {
-        assertThat(new Problem1(10).solve(), is(3+5+6+9L));
-        assertThat(new Problem1(1000).solve(), is(233168L));
+        assertThat(new Problem1().solve(10), is(3+5+6+9L));
+        assertThat(new Problem1().solve(1000), is(233168L));
+    }
+
+    @Test
+    public void solveProblemUsingCalculus() {
+        assertThat(new Problem1().solveUsingCalculus(10), is(3+5+6+9L));
+        assertThat(new Problem1().solveUsingCalculus(1000), is(233168L));
+    }
+
+    @Test
+    public void canFindLastTermInAnArithmeticSequence() {
+        assertThat(new Problem1().findLastTerm(3, 10), is(9));
+        assertThat(new Problem1().findLastTerm(3, 1000), is(999));
+        assertThat(new Problem1().findLastTerm(5, 1000), is(995));
+    }
+
+    @Test
+    public void canFindSumOfTermsInAnArithmeticSequence() {
+        assertThat(new Problem1().findSumOfTerms(3, 9, 3), is(18L));
+        assertThat(new Problem1().findSumOfTerms(3, 999, 333), is(166833L));
+    }
+
+    @Test
+    public void canFindNumberOfTermsInAnArithmeticSequence() {
+        assertThat(new Problem1().findNumberOfTerms(3, 10, 3), is(3));
+        assertThat(new Problem1().findNumberOfTerms(3, 999, 3), is(333));
     }
 
 }
